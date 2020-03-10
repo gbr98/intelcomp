42a1d8e8fa102b6de050bd48de3f12496b72582d
gabriel <gabrieloliveira@ice.ufjf.br>
2020-03-10 18:52:50 -0300

    salas

--- salas/salas.cpp
+++ salas/salas.cpp
@@ -1,8 +1,25 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<vector.h>
 
 using namespace std;
 
+typedef struct {
+	int n,
+	int* up,
+	int* dn,
+	float fitness
+} Solution;
+
+float evaluate(Solution x, float** A, float* tam){
+	float** dist = new float[x.n][x.n];
+	for(int i = 0; i < x.n; i++){
+		for(int j = 0; j < x.n; j++){
+			dist[i][j] = 
+		}
+	}
+}
+
 int main(){
-	
+
 }
