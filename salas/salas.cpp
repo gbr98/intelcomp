fad3298e30aec476a894e369f9f77f15d9a05ba4
Gabriel Oliveira <gabrieloliveira@ice.ufjf.br>
2020-03-10 22:26:31 -0300

    instances

--- salas/salas.cpp
+++ salas/salas.cpp
@@ -8,18 +8,45 @@ typedef struct {
 	int n,
 	int* up,
 	int* dn,
+	int upn,
+	int dnn,
 	float fitness
 } Solution;
 
 float evaluate(Solution x, float** A, float* tam){
-	float** dist = new float[x.n][x.n];
+	float cost = 0;
+	float* pos_x = new float[x.n];
 	for(int i = 0; i < x.n; i++){
-		for(int j = 0; j < x.n; j++){
-			dist[i][j] = 
+		int found = 0;
+		float sum_x = 0;
+		for(int j = 0; j < x.upn; j++){
+			if(x.up[j] == i){
+				pos_x[i] = sum_x+tam[i]*0.5;
+				found = 1;
+				break;
+			}
+			sum_x += tam[x.up[j]];
+		}
+		if(!found){
+			sum_x = 0;
+			for(int j = 0; j < x.dnn; j++){
+				if(x.dn[j] == i){
+					pos_x[i] = sum_x+tam[i]*0.5;
+					found = 1;
+					break;
+				}
+				sum_x += tam[x.dn[j]];
+			}
 		}
 	}
+	for(int i = 0; i < x.n; i++){
+		for(int j = 0; j < x.n; j++){
+			cost += fabs(pos_x[j]-pos_x[i])*A[i][j];
+		}
+	}
+	return cost;
 }
 
 int main(){
-
+	
 }
